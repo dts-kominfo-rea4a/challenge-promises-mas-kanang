3e9c227504f0c6fc526d25c3a095a66d5a48364b
Ahmad Muzaki Munir <ahmadmuzakimunir@gmail.com>
2022-09-19 14:47:14 +0700

    edit promise.js

--- promise.js
+++ promise.js
@@ -1,7 +1,15 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  
+  const TheaterIXX = await promiseTheaterIXX();
+  const TheaterVGC = await promiseTheaterVGC();
+  const semuaFilm = [...TheaterIXX, ...TheaterVGC];
+  const result = semuaFilm.filter((e) => e.hasil === emosi);
+  return result.length;
+
+};
 
 module.exports = {
   promiseOutput,
